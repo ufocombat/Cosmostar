2c2d59f74964e4d46d990de4f66a1615dd679316
ufocombat <ufocombat@MacBook-Pro-ufocombat-2.local>
2019-11-14 16:16:23 +0300

    Initial commit

---
+++ Cosmostar/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cosmostar.Models;
+
+namespace Cosmostar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Order(OrderItemClass order)
+        {
+            ViewBag.Description = order.Description;
+            ViewBag.Quantity = order.Quantity;
+            ViewBag.OrderTime = order.OrderTime;
+
+            var sum = order.Quantity * 25;
+            ViewBag.Sum = sum;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cosmostar/Models/OrderItemClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Cosmostar.Models
+{
+    public class OrderItemClass
+    {
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string OrderTime { get; set; }
+    }
+}
